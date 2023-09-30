f22bb802f06f932acf38b0968f281e713e7bbd2a
Tobias Nieva <tobikapo666@gmail.com>
2023-09-30 17:24:39 -0300

    Crear formulario de acceso

---
+++ Capa de presentacion/FormAcceso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Control_Gym.Capa_de_presentacion
+{
+    public partial class FormAcceso : Form
+    {
+        public FormAcceso()
+        {
+            InitializeComponent();
+        }
+    }
+}
