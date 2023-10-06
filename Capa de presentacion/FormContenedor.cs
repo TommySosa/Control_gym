ecd9b66bdb6a678e9f3e60f05ad3f5ff329b06ba
Elias Ortiz <noseqponer2016@gmail.com>
2023-10-05 22:05:37 -0300

    Cree el form principal

--- Capa de presentacion/FormContenedor.cs
+++ Capa de presentacion/FormContenedor.cs
@@ -107,13 +107,13 @@ namespace Control_Gym
 
         private void btnInicio_Click(object sender, EventArgs e)
         {
-            //AbrirFormEnPanel(new FormPrincipal());
+            AbrirFormEnPanel(new FormPrincipal());
         }
 
         private void FormContenedor_Load(object sender, EventArgs e)
         {
             btnlogoInicio_Click(null, e);
-            //AbrirFormEnPanel(new FormPrincipal());
+            AbrirFormEnPanel(new FormPrincipal());
         }
 
         private void MenuVertical_Paint(object sender, PaintEventArgs e)
