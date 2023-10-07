482e65a00625ed8932c224462a97777ee9bcbe76
TommySosa <tomas.facundo.sosa@gmail.com>
2023-10-06 22:29:41 -0300

    Metodo insert de membresias

---
+++ Capa de datos/CTipoMembresiaD.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Control_Gym.Capa_logica;
+
+namespace Control_Gym.Capa_de_datos
+{
+    internal class CTipoMembresiaD
+    {
+        public List<CTipoMembresia> traerTipos()
+        {
+            ConexionBD conexionBD = ConexionBD.Instancia;
+            List<CTipoMembresia> tipos_membresias = new List<CTipoMembresia>();
+            try
+            {
+                conexionBD.AbrirConexion();
+                string query = "select cod_tipo_membresia, nombre from tipos_membresias";
+                SqlCommand comando = new SqlCommand(query, conexionBD.AbrirConexion());
+                SqlDataReader reader = comando.ExecuteReader();
+                {
+                    while (reader.Read())
+                    {
+                        int cod_tipo_membresia = Convert.ToInt32(reader["cod_tipo_membresia"]);
+                        string nombre = reader["nombre"].ToString();
+                        tipos_membresias.Add(new CTipoMembresia(cod_tipo_membresia, nombre));
+                    }
+                }
+                return tipos_membresias;
+            }
+            catch
+            {
+                MessageBox.Show("Error al traer los tipos");
+                return null;
+            }
+            finally
+            {
+                conexionBD.CerrarConexion();
+            }
+        }
+    }
+}
---
+++ Capa de datos/ConexionBD.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Control_Gym.Capa_de_datos;
+
+namespace Control_Gym.Capa_de_datos
+{
+    public class ConexionBD
+    {
+        private static ConexionBD instancia;
+        private string cadenaConexion = "Server=localhost\\SQLEXPRESS;Database=control_gym;Trusted_Connection=True;";
+
+        private SqlConnection conexion;
+
+        private ConexionBD()
+        {
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+        public static ConexionBD Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new ConexionBD();
+                }
+                return instancia;
+            }
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
